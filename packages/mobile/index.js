3a14fb9441d1382d2b97c40ba00395e3eee3ea3b
Henry Will <99243374+henry-will@users.noreply.github.com>
2023-01-20 17:11:25 +0900

    npx react-native init kbant --template react-native-template-typescript && mv kbant mobile

---
+++ packages/mobile/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from '@kbant/appc';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
